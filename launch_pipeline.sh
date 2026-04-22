68220d46bc70d029dce97d57d144259386345e13
Ofek Vilkerstone <ofekvs18@gmail.com>
2026-04-22 19:26:39 +0300

    add

--- launch_pipeline.sh
+++ launch_pipeline.sh
@@ -5,8 +5,8 @@
 DISEASE=${1:?Usage: bash launch_pipeline.sh <disease> [--skip-bq]}
 SKIP_BQ=${2:-""}
 
-CPU_JOB=$(sbatch --parsable pipeline_cpu.sbatch "$DISEASE" "$SKIP_BQ")
+CPU_JOB=$(sbatch --parsable --export=DISEASE="$DISEASE",SKIP_BQ="$SKIP_BQ" pipeline_cpu.sbatch)
 echo "Submitted CPU job $CPU_JOB for disease=$DISEASE"
 
-GPU_JOB=$(sbatch --parsable --dependency=afterok:"$CPU_JOB" pipeline_gpu.sbatch "$DISEASE")
+GPU_JOB=$(sbatch --parsable --dependency=afterok:"$CPU_JOB" --export=DISEASE="$DISEASE" pipeline_gpu.sbatch)
 echo "Submitted GPU job $GPU_JOB for disease=$DISEASE (depends on $CPU_JOB)"
